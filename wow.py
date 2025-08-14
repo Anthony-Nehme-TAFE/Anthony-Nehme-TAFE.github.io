facf94772e3368e94da8f6a680a6d4b5854adfc9
anthony.nehme7@studytafensw.edu.au <fasf2fvh435herregwqg34er>
2025-08-14 04:16:15 +0000

    I wrote a bubble sorting argorithem.

---
+++ wow.py
@@ -0,0 +1,13 @@
+print("Crazy? I was crazy once, they locked me in a room, a rubber room, a rubber room full of rats, and the rats made me crazy.")
+sorted = False ##I feel like bubble sorting today false
+nums = [12,1,3,9,353453,2,1,1,5,3,6436,23,73,7237,325524353,1,123,1235,125332,6743,854]
+while True:
+    sorted = True
+    for i in range(0,(len(nums)-1)):
+        if nums[i]>nums[i+1]:
+            sorted = False
+            tmp=nums[i+1]
+            nums[i+1]=nums[i]
+            nums[i]=tmp
+    if sorted:break
+print(nums)
