315e2861232206c80523793eaf261d2b4b14a445
Anthony-Nehme-TAFE <anthony.nehme7@studytafensw.edu.au>
2025-08-21 15:44:26 +1000

    smaller number in reandom number

--- wow.py
+++ wow.py
@@ -1,5 +1,5 @@
 sorted = False ##I feel like bubble sorting today false
-nums = [12,1,3,9,353453,2,1,1,5,3,6436,23,73,7237,325524353,1,123,1235,125332,6743,854]
+nums = [12,1,3,9,353453,2,1,1,5,3,6436,23,73,7237,325524353,1,123,1235,1252,6743,854]
 while True:
     sorted = True
     for i in range(0,(len(nums)-1)):
